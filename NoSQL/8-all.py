269950f7afab16a9401e89919382a54629acbe76
KhairiTab <taboubikhairi68@gmail.com>
2024-05-12 14:56:46 +0100

    list add documents in python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,15 +2,6 @@
 def list_all(mongo_collection):
     """
     Lists all documents in a MongoDB collection.
-
-    Returns:
-        List of all documents in the collection.
-        Returns an empty list if no documents are found.
     """
-    # Find all documents in the collection
-    all_documents = mongo_collection.find()
 
-    # Convert cursor to list of documents
-    document_list = list(all_documents)
-
-    return document_list
+    return mongo_collection.find()
