c62e5af338a6fa14d534b61159a3791580b2d4f0
KhairiTab <taboubikhairi68@gmail.com>
2024-05-12 15:31:29 +0100

    schools_by_topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
-"""
-    Returns the list of schools having a specific topic.
-"""
+"""returns the list of school having a specific topic"""
 
 
 def schools_by_topic(mongo_collection, topic):
-
-    """   Returns:
-        A list of dictionaries representing schools having the specific topic.
-    """
-    collection = mongo_collection.find({"topic": topic})
-    return list(collection)
+    """returns the list of school having a specific topic"""
+    return mongo_collection.find({"topics": topic})
