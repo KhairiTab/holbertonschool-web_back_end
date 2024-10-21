0e74a91cf95348ed68ce1b8fd6977a3edbad0025
KhairiTab <taboubikhairi68@gmail.com>
2024-10-21 13:38:59 +0100

    fix code

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,6 @@
 // Define a function named 'displayMessage' that, when called, will print a message to the console
-function displayMessage(){
-    console.log('Hello NodeJS!')
+function displayMessage(str){
+    console.log(str)
 }
 
-// Call the function 'displayMessage' to execute its code and display the message
-displayMessage();
+module.export = displayMessage;
