f3545f643eea916ee20cc47f2f4f2f98bd70da84
KhairiTab <taboubikhairi68@gmail.com>
2024-10-21 13:35:49 +0100

    Executing basic javascript with Node JS

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,7 @@
+// Define a function named 'displayMessage' that, when called, will print a message to the console
 function displayMessage(){
     console.log('Hello NodeJS!')
 }
 
+// Call the function 'displayMessage' to execute its code and display the message
 displayMessage();
