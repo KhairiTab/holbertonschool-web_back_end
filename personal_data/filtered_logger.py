64675772597a09060ae95389e07cff65489300cf
KhairiTab <taboubikhairi68@gmail.com>
2025-02-20 17:16:42 +0100

    Personal data

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""filtered logger"""
+
+import logging
+import mysql.connector
+import os
+import re
+from typing import List
+
+PII_FIELDS = (
+    "name",
+    "email",
+    "phone",
+    "ssn",
+    "password",
+)
+
+
+class RedactingFormatter(logging.Formatter):
+    """class that inherits from built-in logging.Formatter"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initializes the RedactingFormatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats records and filters sensitive info"""
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+        )
+        return super().format(record)
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        pattern = f"{field}=[^{separator}]+"
+        replacement = f"{field}={redaction}"
+        message = re.sub(pattern, replacement, message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """logger function"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """returns a connector to the db"""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST ", "localhost")
+    db = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    connection = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db,
+    )
+    return connection
+
+
+def main() -> None:
+    """reads and filters data"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+
+    for row in cursor.fetchall():
+        message = (
+            f"name={row[0]}; email={row[1]}; phone={row[2]};"
+            f"ssn={row[3]}; password={row[4]}; ip={row[5]};"
+            f"last_login={row[6]}; user_agent={row[7]};"
+        )
+        logger.info(message)
+
+    cursor.close()
+    db.close()
+
+    if __name__ == "__main__":
+        main()
