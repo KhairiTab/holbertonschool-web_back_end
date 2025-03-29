79384d8c976e3cd8503739488bd10abfe424b294
KhairiTab <taboubikhairi68@gmail.com>
2025-03-29 20:54:25 +0000

    Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Writing strings to Redis, Reading from Redis and recovering original type,
    Incrementing values, Storing lists, Retrieving lists"""
-from typing import Union, Any
+from typing import Union, Callable, Optional, Any
 import redis
 import uuid
 
@@ -23,3 +23,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable[[bytes], Any]] = None
+    ) -> Union[str, bytes, int, float, None]:
+        """Retrieve data from Redis using a key
+        and optionally apply a conversion function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve and decode data from Redis as a string."""
+        data = self._redis.get(key)
+        return data.decode("utf-8") if data else None
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve and decode data from Redis as an integer."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        try:
+            return int(data.decode("utf-8"))
+        except (ValueError, AttributeError):
+            return 0
